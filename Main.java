6184a4efe07991b8e479aa5862a7003647d3bd84
Sonu kumar kushwaha <flyingsonu736@gmail.com>
2021-04-24 01:35:48 +0530

    updated

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main{
 
-	public static void main(String[] args){
+	public static void main(String args[]){
 
 		System.out.println("Hello from Sonu Kumar");
 		Welcome.sayName();
 		Welcome.sayAge();
+		SayHello.hello();
 	}
 
 }
--- sayHello.java
+++ sayHello.java
@@ -2,4 +2,8 @@ public class SayHello {
 	public static void main(String[] args){
 		System.out.println("Welcome from Java!!");
 	}
+
+	public static void hello() {
+		System.out.println("Hello");
+	}
 }
